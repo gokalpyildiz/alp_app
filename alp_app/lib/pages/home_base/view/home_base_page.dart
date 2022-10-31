5bc77bfc45e24b7a157b9f080f29795478dd9e00
gokalpyildiz <50711026+gokalpyildiz@users.noreply.github.com>
2022-10-31 03:07:09 +0300

    added all launches page

--- alp_app/lib/pages/home_base/view/home_base_page.dart
+++ alp_app/lib/pages/home_base/view/home_base_page.dart
@@ -1,5 +1,6 @@
 import 'package:alp_app/pages/home_base/bloc/home_base_bloc.dart';
 import 'package:alp_app/pages/home_base/bloc/home_base_event.dart';
+import 'package:alp_app/pages/spacex/view/all_launches_page.dart';
 import 'package:alp_app/pages/spacex/view/spacex_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -7,7 +8,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 // ignore: must_be_immutable
 class HomeBasePage extends StatelessWidget {
   HomeBasePage({Key? key, required this.pageIndex}) : super(key: key);
-  final _pageNavigation = [const SpacexPage(), const SpacexPage()];
+  final _pageNavigation = [const SpacexPage(), AllLaunchesPage()];
   int pageIndex;
 
   @override
@@ -31,10 +32,10 @@ class HomeBasePage extends StatelessWidget {
                 backgroundColor: Colors.white,
                 items: const <BottomNavigationBarItem>[
                   BottomNavigationBarItem(
-                    icon: Icon(Icons.abc),
+                    icon: Icon(Icons.rocket),
                     label: 'Last Launch',
                   ),
-                  BottomNavigationBarItem(icon: Icon(Icons.abc), label: 'All Launches')
+                  BottomNavigationBarItem(icon: Icon(Icons.rocket_launch_rounded), label: 'All Launches')
                 ]),
           );
         },
---
+++ alp_app/lib/pages/spacex/view/all_launches_page.dart
@@ -0,0 +1,110 @@
+import 'package:alp_app/extensions/context_extensions.dart';
+import 'package:alp_app/product/widgets/busy_indicator/busy_indicator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../product/widgets/constants/image_path_constants/svg_image_path.dart';
+import '../../../product/widgets/nation_text/nation_text.dart';
+import '../bloc/spacex_bloc.dart';
+import '../bloc/spacex_state.dart';
+import '../model/spacex_model.dart';
+import '../viewmodel/spacex_viewmodel.dart';
+
+class AllLaunchesPage extends StatelessWidget {
+  AllLaunchesPage({Key? key}) : super(key: key);
+  var spacexViewModel = SpacexViewModel();
+  var imageHeight = 250.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => SpacexBloc(),
+      child: Scaffold(body: SafeArea(child: BlocBuilder<SpacexBloc, SpacexState>(
+        builder: (context, state) {
+          if (state is SpacexInitialState) {
+            spacexViewModel.fetchLaunchingInfo(context);
+            return busyIndicator();
+          } else {
+            var allLaunches = spacexViewModel.launching;
+            return RefreshIndicator(
+              onRefresh: () async {
+                spacexViewModel.refreshLaunches(context);
+              },
+              child: PageView.builder(
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: context.paddingMediumAll,
+                    child: Column(
+                      children: [
+                        _title((index + 1).toString()),
+                        _rocketName(allLaunches[index]),
+                        _rocketImage(context, allLaunches[index]),
+                        _detailInfo(allLaunches[index], context),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            );
+          }
+        },
+      ))),
+    );
+  }
+
+  Widget _title(String launchNumber) {
+    return NationText(
+      'SpaceX Launch Info $launchNumber',
+      color: Colors.black,
+      fontSize: 25,
+    );
+  }
+
+  Widget _rocketName(SpacexModel launch) => NationText(((launch.name ?? '')));
+
+  Widget _rocketImage(BuildContext context, SpacexModel launch) {
+    return Padding(
+      padding: context.paddingMediumAll,
+      child: (launch.links != null && launch.links?.patch != null && launch.links?.patch?.large != null) == true
+          ? Image.network(
+              launch.links!.patch!.large!,
+              height: imageHeight,
+              errorBuilder: (BuildContext context, Object exception, stackTrace) => const Center(
+                child: NationText(
+                  'Image not found',
+                ),
+              ),
+              loadingBuilder: (BuildContext context, Widget child, loadingProgress) {
+                if (loadingProgress == null) return child;
+                return SizedBox(
+                  height: imageHeight,
+                  child: Center(
+                    child: CircularProgressIndicator(
+                      value: loadingProgress.expectedTotalBytes != null
+                          ? loadingProgress.cumulativeBytesLoaded / loadingProgress.expectedTotalBytes!
+                          : null,
+                    ),
+                  ),
+                );
+              },
+            )
+          : SvgPicture.asset(SVGImagePath.instance.imageNotFoundSVG),
+    );
+  }
+
+  Widget _detailInfo(SpacexModel launch, BuildContext context) {
+    //return (lastLaunch.details != null) == true ? NationText((lastLaunch.details ?? ''), fontSize: 18) : const NationText('Details info not found');
+    return Container(
+        decoration: BoxDecoration(border: Border.all(color: Colors.black.withOpacity(0.2)), borderRadius: context.lowBorderRadius),
+        child: (launch.details != null) == true
+            ? Padding(
+                padding: context.paddingLowAll,
+                child: NationText((launch.details ?? ''), fontSize: 18),
+              )
+            : Padding(
+                padding: context.paddingLowAll,
+                child: const NationText('Details info not found'),
+              ));
+  }
+}
